1456cc622c075387e2605a49dfb7c117e5e7b262
domenicovuttariello <domenicovuttariello05@gmail.com>
2025-03-07 12:17:04 +0100

    login

---
+++ src/components/Login.tsx
@@ -0,0 +1,27 @@
+import imgLogin from '../assets/img.svg'
+
+const Login: React.FC = () => {
+  return (
+    <div className="login-container">
+      <div className="image-section">
+        <img src={imgLogin} alt="Login" />
+      </div>
+      <div className="form-section">
+        <h2>Login</h2>
+        <form>
+          <div className="form-group">
+            <label htmlFor="username">Username</label>
+            <input type="text" id="username" name="username" required />
+          </div>
+          <div className="form-group">
+            <label htmlFor="password">Password</label>
+            <input type="password" id="password" name="password" required />
+          </div>
+          <button type="submit">Login</button>
+        </form>
+      </div>
+    </div>
+  );
+};
+
+export default Login;
---
+++ src/components/Register.tsx
@@ -0,0 +1,72 @@
+import  { useState } from 'react';
+
+const Register: React.FC = () => {
+  const [formData, setFormData] = useState({
+    username: '',
+    email: '',
+    password: '',
+  });
+
+  const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const { name, value } = e.target;
+    setFormData({
+      ...formData,
+      [name]: value,
+    });
+  };
+
+  const handleSubmit = (e: React.FormEvent) => {
+    e.preventDefault();
+
+    console.log('Form submitted:', formData);
+  };
+
+  return (
+    <div className="register-container">
+      <div className="image-container">
+        <img src="img.svg" alt="Register" />
+      </div>
+      <div className="form-container">
+        <h2>Register</h2>
+        <form onSubmit={handleSubmit}>
+          <div className="form-group">
+            <label htmlFor="username">Username</label>
+            <input
+              type="text"
+              id="username"
+              name="username"
+              value={formData.username}
+              onChange={handleChange}
+              required
+            />
+          </div>
+          <div className="form-group">
+            <label htmlFor="email">Email</label>
+            <input
+              type="email"
+              id="email"
+              name="email"
+              value={formData.email}
+              onChange={handleChange}
+              required
+            />
+          </div>
+          <div className="form-group">
+            <label htmlFor="password">Password</label>
+            <input
+              type="password"
+              id="password"
+              name="password"
+              value={formData.password}
+              onChange={handleChange}
+              required
+            />
+          </div>
+          <button type="submit">Register</button>
+        </form>
+      </div>
+    </div>
+  );
+};
+
+export default Register;
