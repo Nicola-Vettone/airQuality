11a615b89643724c3a6d28836e7a5ee0d830e490
Nicola Vettone <nicolavettone57@gmail.com>
2025-03-11 12:24:32 +0100

    transparent

--- src/components/Tools.tsx
+++ src/components/Tools.tsx
@@ -6,13 +6,13 @@ import InputGroup from "react-bootstrap/InputGroup";
 function Tools() {
   return (
     <Container className="d-flex gap-3 align-items-center p-3">
-      <Button className="buttonFilter rounded-pill px-4 py-2">
+      <Button className="buttonSearch rounded-pill px-4 py-2">
         <FunnelFill size={20} />
       </Button>
 
-      <InputGroup className="searchBar">
-        <Form.Control className="rounded-pill py-2 bg-transparent" placeholder="Search..." />
-        <InputGroup.Text className=" bg-transparent border-0">
+      <InputGroup>
+        <Form.Control className="rounded-pill py-2 buttonSearch" placeholder="Search..." />
+        <InputGroup.Text className="bg-transparent">
           <Search style={{ position: "absolute", top: "10px", left: "94%" }} size={20} />
         </InputGroup.Text>
       </InputGroup>
