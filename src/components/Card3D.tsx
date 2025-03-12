4b7751173b0909cae3d2865b0309cf2ce14bdc8a
Nicola Vettone <nicolavettone57@gmail.com>
2025-03-12 17:35:09 +0100

    ok

--- src/components/Card3D.tsx
+++ src/components/Card3D.tsx
@@ -5,26 +5,32 @@ import { Link } from "react-router";
 const models = [
   {
     id: 1,
-    title: "Lorem ipsum dolor sit amet",
+    img: "https://plus.unsplash.com/premium_photo-1671233269780-89a290d75b2a?q=80&w=1932&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+    title: "Cube 3D",
   },
   {
     id: 2,
+    img: "https://plus.unsplash.com/premium_photo-1671233269780-89a290d75b2a?q=80&w=1932&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
     title: "Lorem ipsum dolor sit amet",
   },
   {
     id: 3,
+    img: "https://plus.unsplash.com/premium_photo-1671233269780-89a290d75b2a?q=80&w=1932&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
     title: "Lorem ipsum dolor sit amet",
   },
   {
     id: 4,
+    img: "https://plus.unsplash.com/premium_photo-1671233269780-89a290d75b2a?q=80&w=1932&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
     title: "Lorem ipsum dolor sit amet",
   },
   {
     id: 5,
+    img: "https://plus.unsplash.com/premium_photo-1671233269780-89a290d75b2a?q=80&w=1932&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
     title: "Lorem ipsum dolor sit amet",
   },
   {
     id: 6,
+    img: "https://plus.unsplash.com/premium_photo-1671233269780-89a290d75b2a?q=80&w=1932&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
     title: "Lorem ipsum dolor sit amet",
   },
 ];
@@ -38,11 +44,7 @@ function ModelCards() {
             <Col key={model.id} xs={12} sm={6} md={4}>
               <Link className="text-decoration-none" to={"/cube3D"}>
                 <Card className="shadow-lg border-0 rounded-4 overflow-hidden">
-                  <Card.Img
-                    variant="top"
-                    src="https://plus.unsplash.com/premium_photo-1671233269780-89a290d75b2a?q=80&w=1932&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
-                    className="card-img"
-                  />
+                  <Card.Img variant="top" src={model.img} className="card-img " />
 
                   <Card.Body className="bg-light-green p-3">
                     <Card.Title className="fw-bold text-dark">{model.title}</Card.Title>
