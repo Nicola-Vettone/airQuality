8f0111c93647489f57a1091dea6f4f8dc0eae902
Nicola Vettone <nicolavettone57@gmail.com>
2025-03-12 11:22:19 +0100

    tools

--- src/components/Tools.tsx
+++ src/components/Tools.tsx
@@ -1,6 +1,7 @@
 import { Container } from "react-bootstrap";
 import NavBar from "./NavBar";
 import SearchBar from "./SearchBar";
+import { Download } from "react-bootstrap-icons";
 
 function Tools() {
   return (
@@ -8,6 +9,32 @@ function Tools() {
       <Container fluid className="module3d-container">
         <NavBar />
         <SearchBar />
+      </Container>
+      <Container className=" d-flex flex-column align-item-center justify-content-center mt-5 ">
+        <p className="border border-2 mt-2 rounded-pill py-2 px-4">
+          Lorem, ipsum dolor sit amet consectetur adipisicing elit.
+          <Download style={{ position: "absolute", left: "89%" }} size={22} />
+        </p>
+        <p className="border border-2 mt-2 rounded-pill py-2 px-4">
+          Lorem, ipsum dolor sit amet consectetur adipisicing elit.
+          <Download style={{ position: "absolute", left: "89%" }} size={22} />
+        </p>
+        <p className="border border-2 mt-2 rounded-pill py-2 px-4">
+          Lorem, ipsum dolor sit amet consectetur adipisicing elit.
+          <Download style={{ position: "absolute", left: "89%" }} size={22} />
+        </p>
+        <p className="border border-2 mt-2 rounded-pill py-2 px-4">
+          Lorem, ipsum dolor sit amet consectetur adipisicing elit.
+          <Download style={{ position: "absolute", left: "89%" }} size={22} />
+        </p>
+        <p className="border border-2 mt-2 rounded-pill py-2 px-4">
+          Lorem, ipsum dolor sit amet consectetur adipisicing elit.
+          <Download style={{ position: "absolute", left: "89%" }} size={22} />
+        </p>
+        <p className="border border-2 mt-2 rounded-pill py-2 px-4">
+          Lorem, ipsum dolor sit amet consectetur adipisicing elit.
+          <Download style={{ position: "absolute", left: "89%" }} size={22} />
+        </p>
       </Container>
     </Container>
   );
